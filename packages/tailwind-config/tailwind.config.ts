bca58805f3b3687d7a96092733f8d5bddc86c26c
Irere123 <irere2020@outlook.com>
2023-12-26 10:18:50 +0200

    feat(global): new color scheme

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -4,59 +4,46 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      },
       colors: {
-        transparent: "transparent",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          bg: "var(--primary-bg)",
-          fg: "var(--primary-fg)",
-          "accents-1": "var(--primary-accents-1)",
-          "accents-2": "var(--primary-accents-2)",
-          "accents-3": "var(--primary-accents-3)",
-          "accents-4": "var(--primary-accents-4)",
-          "accents-5": "var(--primary-accents-5)",
-          "accents-6": "var(--primary-accents-6)",
-          "accents-7": "var(--primary-accents-7)",
-          "accents-8": "var(--primary-accents-8)",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        error: {
-          lighter: "var(--error-lighter)",
-          light: "var(--error-light)",
-          DEFAULT: "var(--error)",
-          dark: "var(--error-dark)",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        success: {
-          lighter: "var(--success-lighter)",
-          light: "var(--success-light)",
-          DEFAULT: "var(--success)",
-          dark: "var(--success-dark)",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
         },
-        warning: {
-          lighter: "var(--warning-lighter)",
-          light: "var(--warning-light)",
-          DEFAULT: "var(--warning)",
-          dark: "var(--warning-dark)",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        violet: {
-          lighter: "var(--violet-lighter)",
-          light: "var(--violet-light)",
-          DEFAULT: "var(--violet)",
-          dark: "var(--violet-dark)",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        cyan: {
-          lighter: "var(--cyan-lighter)",
-          light: "var(--cyan-light)",
-          DEFAULT: "var(--cyan)",
-          dark: "var(--cyan-dark)",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        highlight: {
-          purple: "var(--highlight-purple)",
-          magenta: "var(--highlight-magenta)",
-          pink: "var(--highlight-pink)",
-          yellow: "var(--highlight-yellow)",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
+      },
+      borderRadius: {
+        xl: `calc(var(--radius) + 4px)`,
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
     },
     fontFamily: {
@@ -76,26 +63,7 @@ const config: Config = {
       // Modal
       "scale-in": "scale-in 0.2s cubic-bezier(0.16, 1, 0.3, 1)",
       "fade-in": "fade-in 0.3s ease-out forwards",
-      // Input Select
-      "input-select-slide-up":
-        "input-select-slide-up 0.2s cubic-bezier(0.16, 1, 0.3, 1)",
-      "input-select-slide-down":
-        "input-select-slide-down 0.2s cubic-bezier(0.16, 1, 0.3, 1)",
-      // Tooltip
-      "slide-up-fade": "slide-up-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)",
-      "slide-right-fade": "slide-right-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)",
-      "slide-down-fade": "slide-down-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)",
-      "slide-left-fade": "slide-left-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)",
-      // Navigation menu
-      "enter-from-right": "enter-from-right 0.25s ease",
-      "enter-from-left": "enter-from-left 0.25s ease",
-      "exit-to-right": "exit-to-right 0.25s ease",
-      "exit-to-left": "exit-to-left 0.25s ease",
-      "scale-in-content": "scale-in-content 0.2s ease",
-      "scale-out-content": "scale-out-content 0.2s ease",
-      // Accordion
-      "accordion-down": "accordion-down 300ms cubic-bezier(0.87, 0, 0.13, 1)",
-      "accordion-up": "accordion-up 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+
       // Custom wiggle animation
       wiggle: "wiggle 0.75s infinite",
       // Custom spinner animation (for loading-spinner)
@@ -113,67 +81,6 @@ const config: Config = {
         "0%": { opacity: "0" },
         "100%": { opacity: "1" },
       },
-      // Input Select
-      "input-select-slide-up": {
-        "0%": { transform: "translateY(-342px)" },
-        "100%": { transform: "translateY(-350px)" },
-      },
-      "input-select-slide-down": {
-        "0%": { transform: "translateY(0px)" },
-        "100%": { transform: "translateY(8px)" },
-      },
-      // Tooltip
-      "slide-up-fade": {
-        "0%": { opacity: "0", transform: "translateY(2px)" },
-        "100%": { opacity: "1", transform: "translateY(0)" },
-      },
-      "slide-right-fade": {
-        "0%": { opacity: "0", transform: "translateX(-2px)" },
-        "100%": { opacity: "1", transform: "translateX(0)" },
-      },
-      "slide-down-fade": {
-        "0%": { opacity: "0", transform: "translateY(-2px)" },
-        "100%": { opacity: "1", transform: "translateY(0)" },
-      },
-      "slide-left-fade": {
-        "0%": { opacity: "0", transform: "translateX(2px)" },
-        "100%": { opacity: "1", transform: "translateX(0)" },
-      },
-      // Navigation menu
-      "enter-from-right": {
-        "0%": { transform: "translateX(200px)", opacity: "0" },
-        "100%": { transform: "translateX(0)", opacity: "1" },
-      },
-      "enter-from-left": {
-        "0%": { transform: "translateX(-200px)", opacity: "0" },
-        "100%": { transform: "translateX(0)", opacity: "1" },
-      },
-      "exit-to-right": {
-        "0%": { transform: "translateX(0)", opacity: "1" },
-        "100%": { transform: "translateX(200px)", opacity: "0" },
-      },
-      "exit-to-left": {
-        "0%": { transform: "translateX(0)", opacity: "1" },
-        "100%": { transform: "translateX(-200px)", opacity: "0" },
-      },
-      "scale-in-content": {
-        "0%": { transform: "rotateX(-30deg) scale(0.9)", opacity: "0" },
-        "100%": { transform: "rotateX(0deg) scale(1)", opacity: "1" },
-      },
-      "scale-out-content": {
-        "0%": { transform: "rotateX(0deg) scale(1)", opacity: "1" },
-        "100%": { transform: "rotateX(-10deg) scale(0.95)", opacity: "0" },
-      },
-      // Accordion
-      "accordion-down": {
-        from: { height: "0" },
-        to: { height: "var(--radix-accordion-content-height)" },
-      },
-      "accordion-up": {
-        from: { height: "var(--radix-accordion-content-height)" },
-        to: { height: "0" },
-      },
-      // Custom wiggle animation
       wiggle: {
         "0%, 100%": {
           transform: "translateX(0%)",
