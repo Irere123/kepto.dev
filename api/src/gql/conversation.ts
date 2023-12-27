d64e6d93553b38e33c6c2fe3c083986505a377db
Irere123 <irere2020@outlook.com>
2023-12-27 10:10:40 +0200

    feat(api): get conversations

--- api/src/gql/conversation.ts
+++ api/src/gql/conversation.ts
@@ -1,18 +1,59 @@
 import { GraphQLError } from "graphql";
 import { GQLContext } from "../lib/types";
-import { and, conversation, db, eq, or } from "@kepto/db";
+import { and, conversation, db, eq, or, sql } from "@kepto/db";
 
 export const typeDefs = /* GraphQL */ `
   type Conversation {
     id: ID!
+    avatarUrl: String!
+    displayName: String!
+    message: DirectMessage
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
   type Mutation {
     createConv(id: ID!): Conversation!
   }
+
+  type Query {
+    conversations: [Conversation!]
+  }
 `;
 
 export const resolvers = {
+  Query: {
+    conversations: async (_parent: unknown, _args: {}, ctx: GQLContext) => {
+      if (!ctx.user) {
+        throw new GraphQLError("Not authenticated");
+      }
+
+      const convs = await db.execute(sql`
+      select
+      case
+        when u.id = co."userId1" then co.read2
+        else co.read1
+      end "read",
+      co.id "id",
+      u.id "userId", u."avatarUrl", u."displayName", date_part('epoch', co."createdAt") * 1000 "createdAt",
+      (select json_build_object('text',
+      case when char_length(text) > 40
+      then substr(text, 0, 40) || '...'
+      else text
+      end
+      , 'createdAt', date_part('epoch', m."createdAt")*1000)
+      from direct_messages m
+      where (m."userId" = co."userId1" or m."userId" = co."userId2")
+      order by m."createdAt" desc limit 1) message
+      from conversations co
+      inner join "users" u on u.id != ${ctx.user.id} and (u.id = co."userId1" or u.id = co."userId2")
+      where (co."userId1" = ${ctx.user.id} or co."userId2" = ${ctx.user.id})
+      limit 150
+      `);
+
+      return convs.rows;
+    },
+  },
   Mutation: {
     createConv: async (
       _parent: unknown,
